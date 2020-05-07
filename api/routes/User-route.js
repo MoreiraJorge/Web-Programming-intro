d98d36886e1bf87def47e8c286bd43d5a5525eee
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-05-07 17:45:32 +0100

    model bug

--- api/routes/User-route.js
+++ api/routes/User-route.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 var User = require("../controllers/UserController")
 
-//List tests
+//List users
 router.get('/userList',function(req, res){
     User.listUsers(req,res)
 })
 
-//Create covid test
+//Create user
 router.post('/create', function(req,res){
     User.createUser(req,res)
 })
