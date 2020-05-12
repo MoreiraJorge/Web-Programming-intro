8d81ed32666e847ea4ae781082592f3d7133931e
MARCELO ANTONIO SOARES CARVALHO <8160287@estg.ipp.pt>
2020-05-12 21:51:47 +0100

    added swagger at 80%, auth, upload, download

--- api/controllers/fileController.js
+++ api/controllers/fileController.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 const Covtest = require('../models/Covtest')
-const fileUpload = require('express-fileupload')
+
+
 
 var FileController = {};
 
@@ -32,15 +33,12 @@ FileController.upload = async (req, res) => {
 
 
 FileController.download = async (req, res) =>{
-
     try{
-
         const result = await Covtest.findOne({ code: req.params.id })
         console.log(result.resultFile)
         if (result.resultFile === null || result.resultFile === []) {
             return res.status(404).send(`<h1>NO FILES UPLOADED</h1>`);
         }else{
-
             res.download(result.resultFile);
             console.log("File downloaded")
         }
